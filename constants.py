d62fa0be7568a35cc6ec8ddfe04fbc7a11289f4f
Jason Cruz <jasoncruz98@gmail.com>
2021-09-25 20:26:24 +0800

    added a 'Hello World' line at the top of the file

--- constants.py
+++ constants.py
@@ -1,51 +1,63 @@
+'''
+Hello world
+'''
+
 """
 Simulation Constants
 """
 
 
 class SimColor:
+
+
 """
 Tuples corresponding to RGB colors
 """
-    LIGHT_GREY = (240, 240, 240)
-    DARK_GREY = (30, 30, 50)
-    BLACK = (0, 0, 0)
-    LIMIT_LINE = (80, 80, 100)
-    INFECTED = (210, 100, 140)
-    RECOVERED = (0, 160, 0)
-    UNEXPOSED = (0, 120, 240)
+LIGHT_GREY = (240, 240, 240)
+DARK_GREY = (30, 30, 50)
+BLACK = (0, 0, 0)
+LIMIT_LINE = (80, 80, 100)
+INFECTED = (210, 100, 140)
+RECOVERED = (0, 160, 0)
+UNEXPOSED = (0, 120, 240)
 
 
 class Disease:
+
+
 """
 Constants for disease
 """
-    INFECTED = 0
-    RECOVERED = 1
-    UNEXPOSED = 2
+INFECTED = 0
+RECOVERED = 1
+UNEXPOSED = 2
 
-    DEFAULT_RECOVERY_PERIOD = 340
+DEFAULT_RECOVERY_PERIOD = 340
 
-    COLOR_MAP = {
-        INFECTED: SimColor.INFECTED,
-        RECOVERED: SimColor.RECOVERED,
-        UNEXPOSED: SimColor.UNEXPOSED
-    }
+COLOR_MAP = {
+    INFECTED: SimColor.INFECTED,
+    RECOVERED: SimColor.RECOVERED,
+    UNEXPOSED: SimColor.UNEXPOSED
+}
 
 
 class Screen:
+
+
 """
 Constants for Screen
 """
-    WIDTH = 680
-    HEIGHT = 480
-    FONT_SIZE = 18
-    GRAPH_X_UNIT = 0.8
-    MEDICAL_LIMIT = 50
+WIDTH = 680
+HEIGHT = 480
+FONT_SIZE = 18
+GRAPH_X_UNIT = 0.8
+MEDICAL_LIMIT = 50
 
 """
 Class for initial condition of Patient
 """
+
+
 class InitialCondition:
     POP_UNEXPOSED = 49
     POP_INFECTED = 1
